18c97246cc7d1e21a0b8816ba73c61fd9686e6c3
nemo1560 <signature8100@gmail.com>
2020-07-03 23:27:15 +0700

    1.0

---
+++ app/src/main/java/nemo/httpsutility/MainActivity.java
@@ -0,0 +1,16 @@
+package nemo.httpsutility;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import nemo.d.myhttpsutility.HttpsUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
